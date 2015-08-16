2b5a229ccbd71931b79f278a6c8bc366fd6ea64b
unknown <rahul.midha@L-10Z6152.jasperindia.local>
2015-08-17 00:03:52 +0530

    Register functionality added complete

--- src/main/webapp/WEB-INF/jsp/registerModal.jsp
+++ src/main/webapp/WEB-INF/jsp/registerModal.jsp
@@ -8,7 +8,42 @@
   <div class="modal-body">
 	<div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
 		<p> Be a <b>Bookha</b>!</p>
-		<form name="sentMessage" id="loginForm" novalidate>
+		<form  name="sentMessage" id="registerForm" novalidate>
+			<div class="row control-group">
+				<div class="form-group col-xs-12 floating-label-form-group controls">
+					<label>Name</label>
+					<input type="text" class="form-control" placeholder="Full Name" id="name" required data-validation-required-message="Please enter your full name">
+					<p class="help-block text-danger"></p>
+				</div>
+			</div>
+			<div class="row control-group">
+				<div class="form-group col-xs-12 floating-label-form-group controls">
+					<label>Street</label>
+					<input type="text" class="form-control" placeholder="Street" id="street" required data-validation-required-message="Please enter your street">
+					<p class="help-block text-danger"></p>
+				</div>
+			</div>
+			<div class="row control-group">
+				<div class="form-group col-xs-12 floating-label-form-group controls">
+					<label>City</label>
+					<input type="text" class="form-control" placeholder="City" id="city" required data-validation-required-message="Please enter your city">
+					<p class="help-block text-danger"></p>
+				</div>
+			</div>
+			<div class="row control-group">
+				<div class="form-group col-xs-12 floating-label-form-group controls">
+					<label>State</label>
+					<input type="text" class="form-control" placeholder="State" id="state" required data-validation-required-message="Please enter your state">
+					<p class="help-block text-danger"></p>
+				</div>
+			</div>
+			<div class="row control-group">
+				<div class="form-group col-xs-12 floating-label-form-group controls">
+					<label>Contact No.</label>
+					<input type="tel" class="form-control" placeholder="Contact No." id="contactno" required data-validation-required-message="Please enter your contact no.">
+					<p class="help-block text-danger"></p>
+				</div>
+			</div>
 			<div class="row control-group">
 				<div class="form-group col-xs-12 floating-label-form-group controls">
 					<label>Email Address</label>
@@ -24,7 +59,7 @@
 				</div>
 			</div>
 			<br>
-			<div id="success"></div>
+			<div id="register-result"></div>
 			<div class="row">
 				<div class="form-group col-xs-12">
 					<button type="submit" class="btn btn-default">Login</button>
--- src/main/webapp/WEB-INF/resources/js/index.js
+++ src/main/webapp/WEB-INF/resources/js/index.js
@@ -26,9 +26,8 @@ $( function() {
 	                },
 	                cache: false,
 	                success: function( data ) {
-	                	console.log( data );
 	                	if( data.success ) {
-	                		location.reload();
+	                		$(location).attr('href', '/Bookhe/userProfile')
 	                	} else {
 	                		// Fail message
 		                    $('#login-result').html("<div class='alert alert-danger'>");
@@ -64,3 +63,71 @@ $( function() {
 	    });
 	});
 });
+
+	$(function() {
+	    $("#registerForm input,textarea").jqBootstrapValidation({
+	        preventSubmit: true,
+	        submitError: function($form, event, errors) {
+	            // additional error messages or events
+	        },
+	        submitSuccess: function($form, event) {
+	            event.preventDefault();
+	            var fullName = $("input#name").val();
+	            var street = $("input#street").val();
+	            var city = $("input#city").val();
+	            var state = $("input#state").val();
+	            var contactno = $("input#contactno").val();
+	            var emailId = $("input#email").val();
+	            var password = $("input#password").val();
+
+	            $.ajax({
+	                url: "/Bookhe/register",
+	                type: "POST",
+	                dataType: "JSON",
+	                data: {
+	                	fullName: fullName,
+	                	street: street,
+	                	city: city,
+	                	state: state,
+	                	contactno: contactno,
+	                    emailId: emailId,
+	                    password: password
+	                },
+	                cache: false,
+	                success: function( data ) {
+	                	console.log( data );
+	                	if( data.success ) {
+	                		$(location).attr('href', '/Bookhe/userProfile')
+	                	} else {
+	                		// Fail message
+		                    $('#register-result').html("<div class='alert alert-danger'>");
+		                    $('#register-result > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+		                        .append("</button>");
+		                    $('#register-result > .alert-danger').append("<strong>Sorry, registration failed. Please try again!!");
+		                    $('#register-result > .alert-danger').append('</div>');
+		                    //clear all fields
+		                    $('#registerForm').trigger("reset");
+	                	}
+	                },
+	                error: function( error ) {
+	                    // Fail message
+	                	$('#register-result').html("<div class='alert alert-danger'>");
+	                    $('#register-result > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+	                        .append("</button>");
+	                    $('#register-result > .alert-danger').append("<strong>Sorry, it seems we encountered a server error. Please try again!!");
+	                    $('#register-result > .alert-danger').append('</div>');
+	                    //clear all fields
+	                    $('#registerForm').trigger("reset");
+	                },
+	            })
+	        },
+	        filter: function() {
+	            return $(this).is(":visible");
+	        },
+	    });
+
+	    $("a[data-toggle=\"tab\"]").click(function(e) {
+	        e.preventDefault();
+	        $(this).tab("show");
+	    });
+	});
